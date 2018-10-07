b477bb6ac8bf005f1c28be2365b02465218e8e7b
Guilherme Cdrs <guilherme.cdrsa@gmail.com>
2018-10-06 23:13:18 -0300

    Project Creation

---
+++ ProgMain/src/progmain/ProgMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progmain;
+
+/**
+ *
+ * @author guilh
+ */
+public class ProgMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
